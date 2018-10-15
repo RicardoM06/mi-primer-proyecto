e99ba3e3a27d0f415151d5fb6537402b866d07d2
Ricardo Miguel Hernàndez <20150998@uthh.edu.mx>
2018-10-15 00:28:33 -0500

    BASE project

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        /* middleware este comprueba si existe un inicio de sesion activa*/
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+}
